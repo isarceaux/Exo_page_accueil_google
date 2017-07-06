646dea9b6971b7e2356805215461d83dcead15a5
isarceaux <isarceaux>
2017-07-07 00:19:24 +0200

    Ajout de la redirection vers Google

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 logo_google = document.images.logo_google;
 form=document.getElementById("form");
 button = document.getElementById("display_button");
+searchbutton=document.getElementById("searchbutton");
 
 //At start nothing is displayed
 logo_google.style.display = "none";
@@ -12,10 +13,18 @@ button.onclick = function ()
 	logo_google.style.display = "";
 	form.style.display="";
 	button.style.display="none";
+	console.log("Click done on main button")
 }
 
-//uses the form to do an actual research in Google
-searchtext = document.getElementById("searchtext");
-searchbutton = document.getElementById("searchbutton");
-localStorage.setItem('googlesearch', searchtext);
+//Uses the form to do an actual research in Google
+
+searchbutton.onclick = function ()
+{
+	searchtext = document.getElementById("searchtext");
+	var url = 'http://www.google.com/#q=';
+	url = url + searchtext.value.split(' ').join('+');
+	console.log(url);	
+	window.open(url);
+}
+
 //...to be continued
